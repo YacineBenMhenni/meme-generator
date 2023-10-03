e3e6f828deb3e02889dd2ee9152910ce702cc7f9
Yacine Ben Mhenni <yassoyacine0009@gmail.com>
2023-10-03 21:12:05 +0100

    Refactor state

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Design
+# Meme generator
 
 ![](./src/design/design.png)
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -28,7 +28,7 @@ export default function Meme() {
           Get a new meme image 🖼
         </button>
       </div>
-      <img src={memeImage} className="meme--image" />
+      <img src={meme.randomImage} className="meme--image" />
     </main>
   );
 }
